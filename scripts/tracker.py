8c98eda6dcebf66a7cd55de279033f23ad464b31
sanjana <sanjanasmallya@gmail.com>
2020-05-01 14:49:50 -0700

    added files

---
+++ scripts/tracker.py
@@ -0,0 +1,344 @@
+#!/usr/bin/python
+
+import cv2
+import sys
+from cv_bridge import CvBridge
+import rospy
+import numpy as np
+import message_filters
+import argparse
+from sensor_msgs.msg import Image
+from vision_msgs.msg import BoundingBox2D
+from std_msgs.msg import Float32, Int8
+
+"""
+
+This ROS Node is adapted from Lorenz Stangier's tracker_scale.py ("implements a object tracker that used a depth image
+to resize it's generated bounding box")
+
+Purpose of this ROS Node is to track objects with a CSRT tracker but without depth information and therefore without 
+depth-reliant scaling. bbox size-dependent scaling functionality remains.
+
+It takes the following parameters:
+    ~color_img_topic:
+        The name of the topic this node should subscribe to for receiving the color img
+    ~publish_tracked:
+        Either True or False. If True, an Image where the BBox is lying over the RGB image is being published
+    ~oob_threshold:
+        A integer value that specifies the distance in pixels that the bounding box has to be away from any image
+        border to be invalid
+
+It subscribes to the following topics:
+    /perception/tracker/bboxIn (vision_msgs/BoundingBox2D):
+        The inital bounding box of to object to track.
+        Only the first message on this topic will be used.
+
+    There is another subscriber that depends on the topics ~color_img_topic.
+
+It publishes on the following topics:
+    /perception/tracker/bboxOut (vision_msgs/BoundingBox2D):
+        The current (scaled) bounding box of the object that's being tracked.
+    /perception/tracker/bboxImage (sensor_msgs/Image)
+        The current RGB Image with overlayed BBox (only if enabled)
+    /perception/tracker/status (Int8)
+        The status of the current bounding box (1 = Valid, 0 = Invalid)
+
+"""
+
+class csrt_tracker(object):
+
+    #
+    # Initilization Functions
+    #
+
+    def __init__(self,  tracker="1",debug=False):
+        self.tracker = tracker
+        self.init_variables_hard(debug)
+        self.init_subscribers()
+        self.init_publisher()
+
+
+
+    # for node startup
+    def init_variables_hard(self, debug):
+        print(self.tracker)
+        if not debug:
+            self.bbox_in_topic = rospy.get_param(
+                "~bbox_in_topic", "/mbz2020/perception/roi/rect"
+            )
+            self.color_image_topic = rospy.get_param(
+                "~color_img_topic", "/front_track_camera/fisheye1/camera_raw"
+            )
+            self.publish_result_img = rospy.get_param(
+                "~publish_tracked", "False"
+            )
+
+            self.oob_threshold = rospy.get_param("~oob_threshold", 10)
+            self.max_bbox_ratio = rospy.get_param("~max_bbox_ratio", 1.0)
+        else:
+            self.color_image_topic = "/front_track_camera/fisheye1/camera_raw"
+            self.bbox_in_topic = "/mbz2020/perception/roi/rect"
+            self.publish_result_img = True
+            self.oob_threshold = 10
+            self.max_bbox_ratio = 1.0
+
+        self._bridge = CvBridge()
+
+        self._color_timestamp = -1
+
+        self._current_color_msg = None
+
+        self._inital_bbox = None
+        self._current_bbox = None
+
+        self._original_distance = -1
+        self._current_distance = -1
+        self._previous_distance = -1
+        OPENCV_OBJECT_TRACKERS = {
+            "1": cv2.TrackerCSRT_create,
+            "2": cv2.TrackerKCF_create,
+            "3": cv2.TrackerBoosting_create,
+            "4": cv2.TrackerMIL_create,
+            "5": cv2.TrackerTLD_create,
+            "6": cv2.TrackerMedianFlow_create,
+            "7": cv2.TrackerMOSSE_create
+	        }
+        print("TRAKER CURRENTLY BEING UTILIZED", OPENCV_OBJECT_TRACKERS[self.tracker])
+        self._tracker = OPENCV_OBJECT_TRACKERS[self.tracker]()
+        
+
+        self._is_first_frame = True
+
+        self._has_scale_changed = False
+        self._scale = 1.0
+        self._fallback_scale = 0.4
+        # self._max_ratio = 1.0
+
+        self._last_bbox = None
+
+        self._current_status = 1
+
+    # for easy tracker re-initialization
+    def init_variables_soft(self):
+
+        self._color_timestamp = -1
+
+        self._current_color_msg = None
+
+        self._inital_bbox = None
+        self._current_bbox = None
+
+        self._original_distance = -1
+        self._current_distance = -1
+        self._previous_distance = -1
+
+        self._is_first_frame = True
+
+        self._has_scale_changed = False
+        self._scale = 1.0
+        self._fallback_scale = 0.4
+        # self._max_ratio = 1.0
+
+        self._last_bbox = None
+
+        self._current_status = 1
+
+    def init_subscribers(self):
+        sub_color = message_filters.Subscriber(self.color_image_topic, Image)
+
+        sub_image = rospy.Subscriber(self.color_image_topic, Image, self.got_image)
+
+        sub_bbox = rospy.Subscriber(
+            self.bbox_in_topic, BoundingBox2D, self.got_bounding_box
+        )
+
+    def init_publisher(self):
+        self._pub_bbox = rospy.Publisher(
+            "/perception/tracker/bboxOut", BoundingBox2D, queue_size=10
+        )
+
+        self._pub_result_img = rospy.Publisher(
+            "/perception/tracker/bboxImage", Image, queue_size=10
+        )
+
+        self._pub_status = rospy.Publisher(
+            "/perception/tracker/status", Int8, queue_size=10
+        )
+
+    #
+    # Helper Functions
+    #
+
+    def check_point_oob(self, point, image, threshold):
+        if point[0] < threshold or point[0] > image.shape[1] - threshold:
+            return True
+
+        if point[1] < threshold or point[1] > image.shape[0] - threshold:
+            return True
+
+        return False
+
+    def calculate_bbox_center(self, bbox):
+        center = (bbox[0] + bbox[2] / 2, bbox[1] + bbox[3] / 2)
+        center = tuple([int(x) for x in center])
+
+        return center
+
+    def scale_bbox(self, bbox, factor):
+        width, height = bbox[2], bbox[3]
+
+        width = width * factor
+        height = height * factor
+
+        center = self.calculate_bbox_center(bbox)
+
+        new_bbox = (
+            center[0] - width / 2,
+            center[1] - height / 2,
+            width,
+            height,
+        )
+
+        return tuple([int(x) for x in new_bbox])
+
+    def get_bbox_scale(self, inital_bbox, new_bbox):
+        i_width, i_height = inital_bbox[2], inital_bbox[3]
+        n_width, n_height = new_bbox[2], new_bbox[3]
+
+        width_scale = n_width / i_width
+        height_scale = n_height / i_height
+
+        return (width_scale + height_scale) / 2
+
+    def spin(self):
+        rate = rospy.Rate(30)
+
+        while not rospy.is_shutdown():
+            rate.sleep()
+
+    #
+    # Callback functions for ROS Subscriptions
+    #
+
+    def got_bounding_box(self, boundingBox):
+        self.init_variables_hard(False)
+
+        center = (boundingBox.center.x, boundingBox.center.y)
+        width = boundingBox.size_x
+        height = boundingBox.size_y
+
+        if self._inital_bbox is None:
+            rospy.loginfo("BBox Received")
+            self._inital_bbox = (
+                int(center[0] - width / 2),
+                int(center[1] - height / 2),
+                width,
+                height,
+            )
+
+    def got_image(self, rgb_msg):
+
+        
+        color_image = self._bridge.imgmsg_to_cv2(rgb_msg)
+
+        final_bbox = None
+
+        if self._is_first_frame and self._inital_bbox is not None:
+            rospy.loginfo("Initializing tracker")
+            current_bbox = self._inital_bbox
+
+            bbox_center = self.calculate_bbox_center(current_bbox)
+
+            self._tracker.init(color_image, current_bbox)
+            self._is_first_frame = False
+
+            final_bbox = current_bbox
+
+        elif not self._is_first_frame:
+
+	    ok, self.tracker_suggested_bbox = self._tracker.update(
+                    color_image
+                )
+
+            if ok:
+                final_bbox = self.tracker_suggested_bbox
+
+            else:
+                self._current_status = 0
+                status_message = Int8()
+                status_message.data = self._current_status
+                self._pub_status.publish(status_message)
+
+
+        if final_bbox is not None:
+            self._last_bbox = final_bbox
+
+            width_ratio = float(final_bbox[2]) / float(color_image.shape[1])
+            height_ratio = float(final_bbox[3]) / float(color_image.shape[0])
+
+            if (
+                width_ratio > self.max_bbox_ratio or height_ratio > self.max_bbox_ratio
+            ) and self._scale != self._fallback_scale:
+                rospy.loginfo("Scaling down...")
+
+                self._scale = self._fallback_scale
+                self._has_scale_changed = True
+            elif (
+                width_ratio < self.max_bbox_ratio and height_ratio < self.max_bbox_ratio
+            ) and self._scale == self._fallback_scale:
+                rospy.loginfo("Scaling back up...")
+
+                self._scale = 1.0
+                self._has_scale_changed = True
+
+            center = self.calculate_bbox_center(final_bbox)
+
+            if self.check_point_oob(center, color_image, self.oob_threshold):
+                self._current_status = 0
+
+            bbox_message = BoundingBox2D()
+
+            bbox_message.size_x = final_bbox[2]
+            bbox_message.size_y = final_bbox[3]
+
+            bbox_message.center.theta = 0
+            bbox_message.center.x = final_bbox[0] + final_bbox[2] / 2
+            bbox_message.center.y = final_bbox[1] + final_bbox[3] / 2
+
+            self._pub_bbox.publish(bbox_message)
+
+            status_message = Int8()
+            status_message.data = self._current_status
+            self._pub_status.publish(status_message)
+
+            if self.publish_result_img:
+                final_bbox = tuple([int(i) for i in final_bbox])
+
+                if self._current_status == 1:
+                    cv2.rectangle(color_image, (final_bbox[0], final_bbox[1]), (final_bbox[0]+final_bbox[2], final_bbox[1]+final_bbox[3]), (0, 0, 255), 2)
+                else:
+                    cv2.rectangle(color_image, (final_bbox[0], final_bbox[1]), (final_bbox[0]+final_bbox[2], final_bbox[1]+final_bbox[3]), (255, 0, 0), 2)
+
+                cv2.circle(color_image, center, 3, (255, 0, 0), 2)
+
+                imgmsg = self._bridge.cv2_to_imgmsg(
+                    color_image, encoding="mono8"
+                )
+  
+                self._pub_result_img.publish(imgmsg)
+
+if __name__ == "__main__":
+    rospy.init_node("csrt_tracker")
+    rospy.loginfo("Starting csrt tracker...")
+    myargv = rospy.myargv(argv=sys.argv)
+
+    # When started from roslaunch, argc is > 1
+    if len(sys.argv) > 1:
+        debug = False
+    else:  # When started from VSCode, argc == 1
+        debug = True
+
+    st = csrt_tracker(myargv[1], debug=debug)
+    rospy.loginfo("Running csrt tracker in debug mode: {}".format(debug))
+
+    st.spin()
---
+++ src/roi_selector_gui.cpp
@@ -0,0 +1,317 @@
+#include <ros/ros.h>
+#include <std_msgs/Int8.h>
+#include <std_msgs/String.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/core/types.hpp>
+#include <vision_msgs/BoundingBox2D.h>
+#include <string>
+#include <iostream>
+using namespace std;
+
+class ButtonGUI
+{
+public:
+  int X, Y, Width, Height;
+  std::string ParamName;
+  std::string Command;
+  const cv::Scalar NormalColor = CV_RGB(153,255,255);
+  const cv::Scalar HighlightColor = CV_RGB(153,255,204);
+  cv::Scalar Color = NormalColor;
+  const cv::Scalar FontColor = CV_RGB(0,0,100);
+
+  ButtonGUI(std::string param_name = "", int x = 0, int y = 0, int width = 100, int height = 30) :
+    X(x), Y(y), Width(width), Height(height), ParamName(param_name)
+  {
+    Command = "rosrun dynamic_reconfigure dynparam set --timeout=2 /vs_mission State " + ParamName;
+  }
+
+  bool CheckInside(int x, int y)
+  {    return (x >= X && x <= X + Width && y >= Y && y <= Y + Height);  }
+
+  void Hover(int x, int y)
+  {    Color = CheckInside(x, y)? HighlightColor : NormalColor;   }
+
+};
+
+static const std::string MAIN_WINDOW = "Camera Feed";
+static const std::string TRACKING_WINDOW = " Tracking";
+cv::Rect2i roi_rect = cv::Rect2i(0, 0, 50, 50);
+cv::Rect2i selected_rect;
+bool roi_selected = false;
+bool roi_sent = false;
+cv::Mat selected_image;
+cv::Size2i image_size;
+cv::Size2i window_size;
+  std::vector<ButtonGUI> buttons;
+
+const int status_bar_height = 30;
+const int status_bar2_height = 50;
+const int separator_width = 5;
+const std::string subscribed_topic = "/camera";
+const std::string publish_topic = "/perception/roi/";
+const std::string bbox_topic = "/perception/tracker/status";
+const std::string bbox_status0 = "Please re-select ROI";
+const std::string bbox_status1 = "Currently tracking";
+const std::string default_status_text = "Left Click: select region  -  Mouse Wheel: change region size  -  Esc Key: Exit";
+const std::string selected_status_text = "Published to " + publish_topic + "  -  Right Click: deselect region  -  " + default_status_text;
+const std::string state_topic = "/vs_mission/cmd_state";
+const std::string tracked_bbox_topic = "/perception/tracker/bboxImage";
+
+std::string action_status_text = "";
+std::string bbox_status_text = bbox_status0;
+ros::Publisher state_pub_;
+
+class ROISelector
+{
+private:
+  ros::NodeHandle nh_;
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Subscriber bbox_img;
+  image_transport::Publisher image_pub_;
+  ros::Publisher rect_pub_;
+  ros::Subscriber bbox_sub_;
+  
+public:
+
+  static void PrintCenteredText(cv::Mat img, std::string text, int font, float size, cv::Scalar color, int x, int y, int w, int h)
+  {
+    int text_baseline = 0;
+    cv::Size text_size = cv::getTextSize(text, font, size, 1, &text_baseline);
+    int text_x = x + (w - text_size.width) / 2;
+    int text_y = y + text_size.height / 2 + h / 2;
+    cv::putText(img, text, cv::Point(text_x, text_y), font, size, color, 1, 8);
+  }
+
+  static void MouseEventHandler(int event, int x, int y, int flags, void* userdata)
+  {
+    if (event == cv::EVENT_RBUTTONDOWN)
+    {
+      roi_selected = false;
+    }
+    else if (event == cv::EVENT_LBUTTONDOWN)
+    {
+      if (x < image_size.width && y < image_size.height)
+      {
+        roi_selected = true;
+        roi_sent = false;
+        if (x >= 0 && y >= 0)
+        {
+          roi_rect.x = x - roi_rect.width / 2;
+          roi_rect.y = y - roi_rect.height / 2;
+        }
+        selected_rect = roi_rect;
+      }
+      else
+      {
+        for (int i = 0; i < (int)buttons.size(); ++i)
+        {
+          if (buttons[i].CheckInside(x, y))
+          {
+            // system((buttons[i].Command + " &").c_str());
+            // ROS_WARN("GUI: Activating %s state...", buttons[i].ParamName.c_str());
+            std_msgs::String msg;
+            msg.data = buttons[i].ParamName;
+            state_pub_.publish(msg);
+            action_status_text = "Activating state: " + buttons[i].ParamName;
+          }
+        }
+      }
+    }
+    else if (event == cv::EVENT_MOUSEHWHEEL)
+    {
+      int diff = 20;
+      if (flags > 0) diff = -diff;
+
+      if (diff > 0 || roi_rect.width > -diff) roi_rect.width += diff;
+      if (diff > 0 || roi_rect.height > -diff) roi_rect.height += diff;
+    }
+
+    if (x >= 0 && y >= 0)
+    {
+      roi_rect.x = x - roi_rect.width / 2;
+      roi_rect.y = y - roi_rect.height / 2;
+    }
+
+    for (int i = 0; i < (int)buttons.size(); ++i)
+      buttons[i].Hover(x, y);
+  }
+
+  void PrintStatus(cv::Mat img_image, const char text[])
+  {
+    int x = 0;
+    int y = window_size.height - status_bar_height - status_bar2_height;
+    int w = window_size.width / 2;
+    int h = status_bar2_height;
+    cv::rectangle(img_image, cv::Rect2i(x + 2, y + 2, w - 4, h - 4), cv::Scalar::all(255), 1, 8);
+    PrintCenteredText(img_image, bbox_status_text, cv::FONT_HERSHEY_COMPLEX, 0.8, CV_RGB(255, 255, 0), x, y, w, h);
+
+    x = window_size.width / 2;
+    cv::rectangle(img_image, cv::Rect2i(x + 2, y + 2, w - 4, h - 4), cv::Scalar::all(255), 1, 8);
+    PrintCenteredText(img_image, action_status_text, cv::FONT_HERSHEY_COMPLEX, 0.5, CV_RGB(205, 102, 102), x, y, w, h);
+
+    x = 0;
+    y = window_size.height - status_bar_height;
+    w = window_size.width;
+    h = status_bar_height;
+    cv::rectangle(img_image, cv::Rect2i(x + 2, y + 2, w - 4, h - 4), cv::Scalar::all(255), 1, 8);
+    cv::putText(img_image, text, cv::Point(10, window_size.height - status_bar_height / 2 + 2), cv::FONT_HERSHEY_COMPLEX, 0.5, cv::Scalar::all(255), 1, 8);
+  }
+
+  void bboxCallback(const std_msgs::Int8::ConstPtr& msg)
+  { 
+    bbox_status_text = (msg->data == 1) ? bbox_status1 : bbox_status0;
+    
+
+  }
+
+
+  ROISelector()
+    : it_(nh_)
+  { 
+    // Subscribe to input video feed and publish the selection
+    image_sub_ = it_.subscribe(subscribed_topic, 1,
+      &ROISelector::imageCb, this); //subscribes to camera feed 
+    image_pub_ = it_.advertise(publish_topic + "image", 1); //publishes annotated bbox image
+    rect_pub_ = nh_.advertise<vision_msgs::BoundingBox2D>(publish_topic + "rect", 1); //publishes annotated bbox image
+    bbox_sub_ = nh_.subscribe(bbox_topic, 10, &ROISelector::bboxCallback, this);
+    bbox_img = it_.subscribe(tracked_bbox_topic,10, &ROISelector::display_tracked_bbox,this);
+    state_pub_ = nh_.advertise<std_msgs::String>(state_topic, 1);
+
+    // Add the buttons
+    buttons.push_back(ButtonGUI("Wait_hov"));
+    buttons.push_back(ButtonGUI("VS_hov"));
+    buttons.push_back(ButtonGUI("VS_fwd"));
+    buttons.push_back(ButtonGUI("VS_bwd"));
+
+    cv::namedWindow(MAIN_WINDOW);
+    cv::setMouseCallback(MAIN_WINDOW, MouseEventHandler, NULL);
+
+  }
+
+  ~ROISelector()
+  {
+    cv::destroyWindow(MAIN_WINDOW);
+  }
+  void display_tracked_bbox(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr bbox;
+    try
+    {
+      bbox = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::MONO8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    // Calculate the window size
+    image_size.height = bbox->image.rows;
+    image_size.width = bbox->image.cols;
+    cv::Mat frame(image_size.height, image_size.width, bbox->image.type(), CV_RGB(0, 0, 50));
+    bbox->image.copyTo(frame(cv::Rect(0, 0, bbox->image.cols, bbox->image.rows)));
+    cv::imshow(TRACKING_WINDOW, frame);
+
+  }
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    cv_bridge::CvImagePtr cv_ptr;
+    try
+    {
+      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+
+    // Calculate the window size
+    image_size.height = cv_ptr->image.rows;
+    image_size.width = cv_ptr->image.cols;
+    window_size.height = image_size.height + status_bar_height + status_bar2_height;
+    window_size.width = image_size.width * 2 + separator_width;
+    if (!buttons.empty())
+      window_size.height += buttons[0].Height + 2 * separator_width;
+
+    // Calculate the button positions and width
+    if (!buttons.empty())
+    {
+      int buttons_width = (window_size.width - (buttons.size() + 1) * separator_width) / buttons.size();
+      buttons[0].X = separator_width;
+      for (int i = 0; i < (int)buttons.size(); ++i)
+      {
+        buttons[i].Width = buttons_width;
+        if (i > 0) buttons[i].X = buttons[i-1].X + buttons[i-1].Width  + separator_width;
+        buttons[i].Y = image_size.height + separator_width;
+      }
+    }
+
+
+    // Create a new image
+    cv::Mat frame(window_size.height, window_size.width, cv_ptr->image.type(), CV_RGB(0, 0, 50));
+
+    if (roi_selected && !roi_sent)
+    {
+      selected_image = cv_ptr->image.clone();
+
+      image_pub_.publish(cv_ptr->toImageMsg());
+      vision_msgs::BoundingBox2D msg;
+      msg.center.x = selected_rect.x + selected_rect.width / 2;
+      msg.center.y = selected_rect.y + selected_rect.height / 2;
+      msg.center.theta = 0;
+      msg.size_x = selected_rect.width;
+      msg.size_y = selected_rect.height;
+      rect_pub_.publish(msg);
+      roi_sent = true;
+    }
+
+    // Draw the region on the video stream
+    cv::rectangle(cv_ptr->image, roi_rect, CV_RGB(255, 0, 0), 2);
+
+    // Update GUI Window
+    if (roi_selected)
+    {
+      cv::rectangle(selected_image, selected_rect, CV_RGB(0,255,0), 2); //draw the rectangle on the secon(selected_image)
+      selected_image.copyTo(frame(cv::Rect(image_size.width + separator_width, 0, selected_image.cols, selected_image.rows))); //to display this image in the second half of the window
+
+    }
+
+    cv_ptr->image.copyTo(frame(cv::Rect(0, 0, cv_ptr->image.cols, cv_ptr->image.rows)));
+
+    // Draw the GUI Buttons
+    for (int i = 0; i < (int)buttons.size(); ++i)
+    {
+      cv::rectangle(frame, cv::Rect2i(buttons[i].X, buttons[i].Y, buttons[i].Width, buttons[i].Height),
+          buttons[i].Color, cv::FILLED);
+
+      PrintCenteredText(frame, buttons[i].ParamName, cv::FONT_HERSHEY_COMPLEX, 0.5, buttons[i].FontColor,
+          buttons[i].X, buttons[i].Y, buttons[i].Width, buttons[i].Height);
+    }
+
+    // Update the status
+    if (roi_selected)
+      PrintStatus(frame, default_status_text.c_str());
+    else
+      PrintStatus(frame, selected_status_text.c_str());
+
+    cv::imshow(MAIN_WINDOW, frame);
+
+    // Exit the program when ESC is pressed
+    int key_pressed = cv::waitKey(1);
+    if (key_pressed == 27) exit(0);
+  }
+ 
+};
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "roi_selector_gui");
+  ROISelector roi_selector;
+  ros::spin();
+  return 0;
+}
